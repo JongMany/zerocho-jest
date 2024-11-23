82d7e9a8c12764feb1323c11d16a37b1ff36eebe
JongMany <blackberry1114@naver.com>
2024-11-23 15:38:36 +0900

    toMatchObject 테스트 코드 추가

--- src/toMatchObject.spec.ts
+++ src/toMatchObject.spec.ts
@@ -10,4 +10,8 @@ test('클래스 인스턴스 비교는 toMatchObject로 해야한다.', () => {
   expect(obj('hello')).not.toStrictEqual({
     a: 'hello',
   });
+
+  expect([1, 2]).toMatchObject([1, 2]);
+
+  expect({ a: 1, b: 2 }).toMatchObject({ a: 1 }); // 통과
 });
